88f2f394e31408c640e99b6dcedbdb3ad10561c7
ble brice gnoboa <gnoboa@live.fr>
2020-09-28 09:06:31 +0000

    first commit

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using rpg.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        public static Character character = new Character();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(character);
+        }
+    }
+}
---
+++ Models/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rpg.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Frodo";
+        public int HitPoints { get; set; } = 100;
+        public int Strength { get; set; } = 10;
+        public int Defense { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+        public RpgClass Class { get; set; } = RpgClass.knight;
+        public int Fights { get; set; }
+        public int Victories { get; set; }
+        public int Defeats { get; set; }
+    }
+
+    public enum RpgClass
+    {
+        knight = 1,
+        Mage = 2,
+        Cleric = 3
+    }
+}
